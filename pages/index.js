b2ef73c2a6a42566a229bb1e61b3f7c6a5dd59de
Tergene Focus <tergenefocus@gmail.com>
2025-09-17 23:14:45 -0400

    Create  index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: 'system-ui, sans-serif' }}>
+      <h1>TF-Chat</h1>
+      <p>API ready at <code>/api/generate</code></p>
+    </main>
+  );
+  }
